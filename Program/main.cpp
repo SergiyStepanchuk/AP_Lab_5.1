e7f79371e34f92afddc2a13d632a35ee8dcabd38
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-11-11 10:51:58 +0200

    edited

--- Program/main.cpp
+++ Program/main.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 double h(const double& x, const double& y);
 void main() {
-	double p, q;
+	double p, q, m;
 
 	cout << "p = "; cin >> p;
 	cout << "q = "; cin >> q;
 
-	cout << (pow(2, h(abs(p - q), sqrt(abs(p - q)))) +
-		h(1, pow(q, 2) - pow(p, 2))) << endl;
+	m = pow(2, h(abs(p - q), sqrt(abs(p - q)))) +
+		h(1, pow(q, 2) - pow(p, 2));
+
+	cout << m << endl;
 }
 
 double h(const double& x, const double& y) {
